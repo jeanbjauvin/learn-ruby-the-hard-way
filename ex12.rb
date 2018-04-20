dd2544c0024e7ac4faf09b03d9435ea72d9af7a4
Jean Bruno Jauvin <jeanbjauvin@gmail.com>
2018-04-19 22:14:55 -0400

    Completed exercises 6 through 15

---
+++ ex12.rb
@@ -0,0 +1,18 @@
+print "Give me a number: "
+number = gets.chomp.to_f
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_f
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+print "Please pay: "
+amount = gets.chomp.to_f
+
+change = amount * 0.10
+puts "Your change is #{change}"
---
+++ ex13.rb
@@ -0,0 +1,14 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+print "Input a number: "
+num = $stdin.gets.chomp
+
+print "Inputs a second number: "
+# num2 = gets.chomp
+num2 = 2
+
+puts "Num: #{num}, Num2: #{num2}"
